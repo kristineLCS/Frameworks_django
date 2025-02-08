e827f38b863a859b67e479e10fa175eb9a2aa7b3
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-08 21:59:32 +0000

    not much change

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -15,7 +15,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
 @login_required
 def profile(request):
     if request.method == 'POST':
