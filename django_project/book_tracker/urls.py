649bfd7add053eaf21b2e3adb4eab8d1cbb966dc
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-10 17:51:59 +0000

    added postlistview url

--- django_project/book_tracker/urls.py
+++ django_project/book_tracker/urls.py
@@ -12,11 +12,12 @@ from .views import book_search
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('posts/', PostListView.as_view(), name='post-list'),  # Separate URL for posts
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('search/', book_search, name='book_search'), #search bar
+    path('search/', views.book_search, name='book_search'),  # search bar
     path('posts/',views.posts, name='blog-posts'),
 ]
