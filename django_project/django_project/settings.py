563765f80eeb735c8b8d232bac142805993e82fe
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-10 17:54:22 +0000

    added static related codes

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+from django.conf.urls.static import static
 import os
 from dotenv import load_dotenv
 
@@ -133,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
