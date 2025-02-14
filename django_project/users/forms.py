ae19ced62c7f77de46d97574b140036b425beccf
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-14 22:11:05 +0000

    creation of folder form

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+# from .models import Profile
+from .models import Folder
+
 
 
 class UserRegisterForm(UserCreationForm):
@@ -19,3 +21,10 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+# Profile Folders
+class FolderForm(forms.ModelForm):
+    class Meta:
+        model = Folder
+        fields = ['name']
