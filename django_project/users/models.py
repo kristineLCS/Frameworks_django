eee334a232648beb735cda7b7f1dd837d2888065
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-14 22:11:49 +0000

    creation of folder and book folder model

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
 
 User = get_user_model()
 
@@ -8,3 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+class Folder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders')
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f"{self.name} ({self.user.username})"
+
+class BookFolder(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='book_folders')
+    book = models.ForeignKey('book_tracker.Book', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.book.title} in {self.folder.name}"
