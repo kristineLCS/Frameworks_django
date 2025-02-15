c7795929b58505f5ac8b0b3d15e52b5f048e8f77
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-15 20:20:39 +0000

    added static

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 
@@ -50,3 +52,6 @@ urlpatterns = [
         ),
         name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
