41218ec4c3a24fbf222aaf95c07a88876615f768
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-15 15:05:44 +0000

    commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-df7p$63wf1sbctub)&wgwg(fz6afa2vcryf$o@x&6nrn8v9&$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
 
 # Application definition
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
