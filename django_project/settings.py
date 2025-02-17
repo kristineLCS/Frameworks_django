38c8c0ab7cbf3265b7a1a04d31d1959b6ac92e26
Kristine Segovia <kristinesegovia23@gmail.com>
2025-02-17 17:52:11 +0000

    manually setting port

--- django_project/settings.py
+++ django_project/settings.py
@@ -160,3 +160,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+PORT = os.getenv("PORT", "8000")
