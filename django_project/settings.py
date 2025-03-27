bad1f418634bf18744dce1538dac0655332f9043
Kristine Segovia <kristinesegovia23@gmail.com>
2025-03-27 22:01:57 +0000

    small change

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,6 @@ GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'booked-and-busy.onrender.com']
