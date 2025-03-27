0b5c84448d16921483be85940a211af153e978bf
Kristine Segovia <kristinesegovia23@gmail.com>
2025-03-27 21:27:57 +0000

    commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,7 @@ GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("MY_SECRET_KEY')
+SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
